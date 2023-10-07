d6f43a3abc914c834c8e42416a9ed8c980ec6d69
Roma-2 <daniel.romano43@unach.mx>
2023-10-06 23:57:39 -0600

    Agregar archivos de proyecto.

---
+++ practica04 formulario/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace practica04_formulario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbApellido = new System.Windows.Forms.TextBox();
+            this.tbEdad = new System.Windows.Forms.TextBox();
+            this.tbEstatura = new System.Windows.Forms.TextBox();
+            this.tbTelefono = new System.Windows.Forms.TextBox();
+            this.Masculino = new System.Windows.Forms.RadioButton();
+            this.Femenino = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(126, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(242, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(126, 29);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(100, 20);
+            this.tbNombre.TabIndex = 2;
+            // 
+            // tbApellido
+            // 
+            this.tbApellido.Location = new System.Drawing.Point(252, 29);
+            this.tbApellido.Name = "tbApellido";
+            this.tbApellido.Size = new System.Drawing.Size(100, 20);
+            this.tbApellido.TabIndex = 3;
+            // 
+            // tbEdad
+            // 
+            this.tbEdad.Location = new System.Drawing.Point(385, 28);
+            this.tbEdad.Name = "tbEdad";
+            this.tbEdad.Size = new System.Drawing.Size(100, 20);
+            this.tbEdad.TabIndex = 4;
+            // 
+            // tbEstatura
+            // 
+            this.tbEstatura.Location = new System.Drawing.Point(492, 27);
+            this.tbEstatura.Name = "tbEstatura";
+            this.tbEstatura.Size = new System.Drawing.Size(100, 20);
+            this.tbEstatura.TabIndex = 5;
+            // 
+            // tbTelefono
+            // 
+            this.tbTelefono.Location = new System.Drawing.Point(599, 28);
+            this.tbTelefono.Name = "tbTelefono";
+            this.tbTelefono.Size = new System.Drawing.Size(100, 20);
+            this.tbTelefono.TabIndex = 6;
+            // 
+            // Masculino
+            // 
+            this.Masculino.AutoSize = true;
+            this.Masculino.Location = new System.Drawing.Point(115, 56);
+            this.Masculino.Name = "Masculino";
+            this.Masculino.Size = new System.Drawing.Size(85, 17);
+            this.Masculino.TabIndex = 7;
+            this.Masculino.Text = "radioButton1";
+            this.Masculino.UseVisualStyleBackColor = true;
+            // 
+            // Femenino
+            // 
+            this.Femenino.AutoSize = true;
+            this.Femenino.Checked = true;
+            this.Femenino.Location = new System.Drawing.Point(242, 56);
+            this.Femenino.Name = "Femenino";
+            this.Femenino.Size = new System.Drawing.Size(85, 17);
+            this.Femenino.TabIndex = 8;
+            this.Femenino.TabStop = true;
+            this.Femenino.Text = "radioButton1";
+            this.Femenino.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Femenino);
+            this.Controls.Add(this.Masculino);
+            this.Controls.Add(this.tbTelefono);
+            this.Controls.Add(this.tbEstatura);
+            this.Controls.Add(this.tbEdad);
+            this.Controls.Add(this.tbApellido);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbApellido;
+        private System.Windows.Forms.TextBox tbEdad;
+        private System.Windows.Forms.TextBox tbEstatura;
+        private System.Windows.Forms.TextBox tbTelefono;
+        private System.Windows.Forms.RadioButton Masculino;
+        private System.Windows.Forms.RadioButton Femenino;
+    }
+}
+
---
+++ practica04 formulario/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica04_formulario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Obtener los datos de los TextBox
+            string nombres = tbNombre.Text;
+            string apellido = tbApellido.Text;
+            string edad = tbEdad.Text;
+            string estatura = tbEstatura.Text;
+            string telefono = tbTelefono.Text;
+
+            //  Obtener el género seleccionado 
+            string genero = "";
+            if (Masculino.Checked)
+            {
+                genero = "Hombre";
+            }
+            else if (Femenino.Checked)
+            {
+                genero = "Mujer";
+            }
+
+            // Crear una cadena de datos 
+            string datos = $"Nombres: {nombres}\r\nApellidos: {apellido}\r\nEdad: {edad}\r\nEstatura: {estatura}\r\nTelefono{telefono}";
+
+            // Guardar los datos en un archivo de texto 
+            string rutaArchivo = "C:/Users/dan96/OneDrive/3M.txt";
+            bool archivoExiste = File.Exists(rutaArchivo);
+
+            if (archivoExiste == false)
+            {
+                File.WriteAllText(rutaArchivo, datos);
+            }
+            else
+            {
+                using (System.IO.StreamWriter writer = new System.IO.StreamWriter(rutaArchivo, true))
+                {
+                    if (archivoExiste)
+                    {
+                        writer.WriteLine();
+                    }
+                    writer.WriteLine(datos);
+                }
+            }
+            MessageBox.Show("Datos Guardados con éxito: \n\n" + datos, "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tbNombre.Clear();
+            tbApellido.Clear();
+            tbEdad.Clear();
+            tbEstatura.Clear();
+            tbTelefono.Clear();
+            Masculino.Checked = false;
+            Femenino.Checked = false;
+        }
+    }
+    }
+}
